1ab6ad33b9bab77678bebd4484d15ba513c6cb89
Aidan Scannell <scannell.aidan@gmail.com>
2020-08-04 11:59:59 +0100

    Added emacs-profile and emacs-profiles.el

---
+++ .emacs-profiles.el
@@ -0,0 +1,17 @@
+(("vanilla" . ((user-emacs-directory . "~/.dotfiles/vanilla-emacs")))
+
+("spacemacs-old-m" . ((user-emacs-directory . "~/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.dotfiles/spacemacs-old")))))
+
+("spacemacs-old-d" . ((user-emacs-directory . "~/spacemacs/develop")
+                 (env . (("SPACEMACSDIR" . "~/.dotfiles/spacemacs-old")))))
+
+("spacemacs-master" . ((user-emacs-directory . "~/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.dotfiles/spacemacs-base-new")))))
+
+("org-config" . ((user-emacs-directory . "~/spacemacs/develop-org")
+                       (env . (("SPACEMACSDIR" . "~/.dotfiles/spacemacs-org-config")))))
+
+ ("new-config" . ((user-emacs-directory . "~/spacemacs/develop")
+                  (env . (("SPACEMACSDIR" . "~/.dotfiles/spacemacs-base-new"))))))
+
